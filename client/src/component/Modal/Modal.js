2c787adb2bfe60a70776c4e3e71ccb2f7680574e
Andrew Driban <dribaan@habasitusa.net>
2018-11-13 17:17:15 -0500

    will try and work on personal computer

--- client/src/component/Modal/Modal.js
+++ client/src/component/Modal/Modal.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Modal.css';
 import Modal from 'react-bootstrap/lib/Modal';
+import requestForm from '../Form';
 
 class RequestModal extends React.Component {
     constructor(props, context) {
@@ -25,8 +26,24 @@ class RequestModal extends React.Component {
     render() {
         return (
             <div>
-                
+                <Modal show={this.state.show} onHide={this.handleClose}>
+
+                    <Modal.Header closeButton>
+                        <Modal.Title>Add New Request</Modal.Title>
+                    </Modal.Header>
+
+                    <Modal.Body>
+                        <requestForm/>
+                    </Modal.Body>
+
+                    <Modal.Footer>
+                        <Button onClick={this.handleClose}>Close</Button>
+                    </Modal.Footer>
+
+                </Modal>
             </div>
         )
     }
 }
+
+render(<RequestModal/>)
