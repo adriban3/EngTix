46286aa1ced3b3827c669e302092b9ba0ce1320a
Andrew <andrewmd4774@gmail.com>
2018-11-26 14:59:43 -0500

    still just working on passport

--- backend/server.js
+++ backend/server.js
@@ -28,7 +28,7 @@ app.use((req, res, next) => {
     console.log('req.session', req.session)
     return next();
 });
-
+//this is what you just added^^
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
